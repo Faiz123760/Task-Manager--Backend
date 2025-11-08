124acdfb8f7e6df1a27300f03e72f34ecfdd64d7
Mohd Faiz <145976190+Faiz123760@users.noreply.github.com>
2025-11-08 12:41:38 +0530

    Add root route to return server status message

--- api/index.js
+++ api/index.js
@@ -40,3 +40,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Task Manager Backend is running 🚀');
+});
+
